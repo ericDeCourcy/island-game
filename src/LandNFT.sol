db710152e0150999c3a7c56939b7bc0824970336
Eric DeCourcy <eric@openzeppelin.com>
2026-08-27 00:11:36 -0600

    Update, does not compile

--- src/LandNFT.sol
+++ src/LandNFT.sol
@@ -8,9 +8,26 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20Burnable
 
 import "./TicketMachine.sol";
 
-contract LandNFT is Initializable, ERC721EnumerableUpgradeable, OwnableUpgradeable, ReentrancyGuard{
+contract LandNFT is 
+    Initializable, 
+    ERC721EnumerableUpgradeable, 
+    OwnableUpgradeable, 
+    ReentrancyGuard,
+    EpochManager {
 
     ERC20BurnableUpgradeable ticketMachine;
+
+    struct IslandData {
+        // TODO 
+        // mapping of x,y
+        // Terrain data
+        // age
+        // npcs
+        // nonterrain objects (plants, buildings, etc)
+        // lastEpoch
+    }
+
+    mapping(uint tokenId => IslandData) IslandDatum;
 
     constructor() {
         _disableInitializers();     //Implementation should not be allowed to initialize
@@ -38,10 +55,57 @@ contract LandNFT is Initializable, ERC721EnumerableUpgradeable, OwnableUpgradeab
             
             // get current number of NFTs in existence, then mint the next id
             // Always doing this means we always mint the next id in order //TODO AUDIT right??
-            _mint(msg.sender, totalSupply());   //TODO AUDIT - this is not off-by-one right?
+                // may need mint consecutive
+            _mint(msg.sender, totalSupply());   // Calls ERC721Upgradeable._mint()
+                //TODO AUDIT - this is not off-by-one right?    
+                //TODO AUDIT: consider using safemint
         }
     }
 
+    /** 
+     * @dev Updates an island `numEpochs` number of cycles. 
+     * @dev Will stop early if the island is caught up to the current global epoch
+     * @dev Each epoch, random events occur, then chance-timed occurences (eg plant growth), then timed occurrences, then queued user actions if possible 
+     */
+    function processEpochs(uint tokenId, uint numEpochs) public nonReentrant{
+        require(_canProcess(tokenId, msg.sender), "processEpochs: msg.sender cannot process for this tokenId");
+        _attemptUpdateToEpoch(tokenId, IslandDatum[tokenId].lastEpoch + numEpochs);
+    } 
+
+    function processUpToEpoch(uint tokenId, uint finalEpoch) public nonReentrant {
+        require(_canProcess(tokenId, msg.sender), "processUpToEpoch: msg.sender cannot process for this tokenId");
+        _attemptUpdateToEpoch(tokenId, finalEpoch);
+    }
+
+    function queueAction(uint tokenId, uint action, bytes32 aux)
+    {
+        require(_canQueueActions(tokenId, msg.sender), "queueAction: msg.sender cannot queue actions for this tokenId");
+        return _queueActionInEpoch(tokenId, action, IslandDatum[tokenId].lastEpoch + 1);
+    }
+
+    function queueActionInEpoch(uint tokenId, uint action, bytes32 aux, uint epoch)
+    {
+        require(_canQueueActions(tokenId, msg.sender), "queueActionInEpoch: msg.sender cannot queue actions for this tokenId");
+        require(epoch > lastEpoch[tokenId], "queueActionInEpoch: specified epoch has already passed");
+        return _queueActionInEpoch(tokenId, action, epoch);
+    }
+
+    function _attemptUpdateToEpoch(tokenId, finalEpoch)
+    {
+        // this is for the very end of the update round, all changes to be made due to rolls and such
+        Effects[] effects = new Effects[];
+
+        for(i = IslandDatum[tokenId].lastEpoch + 1; i <= finalEpoch; i++)
+        {
+            // do random events
+            // do chanceTimed occurrences
+            Event[] timedEvents = _scanForTimedEvents(tokenId);
+            // do timed occurrences (things like decay or non-infinite effects... tbh this might be ok to remove)
+            // do queued user actions
+
+
+        }
+    }
 
 
 }
